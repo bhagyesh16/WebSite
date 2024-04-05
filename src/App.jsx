f16b1f9aa97303b86b706269a1d160b59a612d07
bhagyesh16 <bhagyeshtrivedi16@gmail.com>
2024-04-05 16:50:23 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
 import styles from './styles'
 import { Stats, Services, Testinomial, Navbar, Home, Contact, Footer, Products, Company } from './components';
-
+import { Analytics } from "@vercel/analytics/react"
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
+  <Analytics id="YOUR_VERCEL_ANALYTICS_ID">
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
-    </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Home />
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Home />
+        </div>
       </div>
-    </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats/>
-        <Services />
-        <Products />
-        <Testinomial />
-        <Company/>
-        <Contact />
-        <Footer />
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Services />
+          <Products />
+          <Testinomial />
+          <Company />
+          <Contact />
+          <Footer />
+        </div>
       </div>
     </div>
-  </div>
+  </Analytics>
 );
 
 export default App
