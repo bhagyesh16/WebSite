7069ea2f95302b15f3e08e858be33d9a691c1af1
bhagyesh16 <bhagyeshtrivedi16@gmail.com>
2024-04-05 16:57:29 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import styles from './styles'
 import { Stats, Services, Testinomial, Navbar, Home, Contact, Footer, Products, Company } from './components';
 import { Analytics } from "@vercel/analytics/react"
 const App = () => (
-  <Analytics id="YOUR_VERCEL_ANALYTICS_ID">
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
-
+      <Analytics id="VERCEL">
+      </Analytics>
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Home />
@@ -28,7 +28,6 @@ const App = () => (
         </div>
       </div>
     </div>
-  </Analytics>
 );
 
 export default App
